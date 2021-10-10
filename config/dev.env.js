d8cd3835d1b33518ede91bded4d3b1076a1150f5
Vũ Khương Duy <vukhuongduy23@gmail.com>
2021-10-10 17:56:48 +0700

    Test tool check code codacy

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  AWS_KEY: 'dfaksjdfhakjsdhfakshdf'
   // SERVERHOST: '"http://back-end:3000"'
 })
 
